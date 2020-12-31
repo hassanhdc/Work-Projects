e4ec71a88197e0925fe3f540693d82afa9829a91
hassanhdc <muhammad.hassan@hyperdatacomputing.com>
2020-12-31 23:09:38 +0500

    Separate drawing functionality into lib.rs

--- gst-draw_on_video/src/lib.rs
+++ gst-draw_on_video/src/lib.rs
@@ -41,6 +41,73 @@ unsafe impl glib::SendUnique for LayoutWrapper {
         self.0.ref_count() == 1
     }
 }
+#[derive(Clone)]
+#[allow(non_camel_case_types)]
+pub struct text {
+    content: String,
+    x: f64,
+    y: f64,
+    rgba: (f64, f64, f64, f64),
+    present_time: (u64, u64),
+    pub(crate) rendered: bool,
+}
+#[derive(Copy, Clone)]
+#[allow(non_camel_case_types)]
+pub struct rect {
+    x: f64,
+    y: f64,
+    width: f64,
+    height: f64,
+    rgba: (f64, f64, f64, f64),
+    present_time: (u64, u64),
+    pub(crate) rendered: bool,
+}
+
+#[derive(Clone)]
+pub enum OverlayElement {
+    Rectangle(rect),
+    Text(text),
+}
+
+impl OverlayElement {
+    pub fn create_rectangle(
+        x: f64,
+        y: f64,
+        width: f64,
+        height: f64,
+        rgba: (f64, f64, f64, f64),
+        present_time: (u64, u64),
+    ) -> Self {
+        let rect = rect {
+            x,
+            y,
+            width,
+            height,
+            rgba,
+            present_time,
+            rendered: false,
+        };
+        OverlayElement::Rectangle(rect)
+    }
+
+    pub fn create_text(
+        content: String,
+        x: f64,
+        y: f64,
+        rgba: (f64, f64, f64, f64),
+        present_time: (u64, u64),
+    ) -> Self {
+        let text = text {
+            content,
+            x,
+            y,
+            rgba,
+            present_time,
+            rendered: false,
+        };
+        OverlayElement::Text(text)
+    }
+}
 
 pub struct VideoContext {}
 
@@ -73,5 +140,128 @@ impl VideoContext {
         src.set_property_from_str("pattern", "smpte");
 
         Ok(pipeline)
+    }
+
+    pub fn draw_on(overlay: gst::Element, overlay_element: Vec<OverlayElement>) {
+        let fontmap = pangocairo::FontMap::new().unwrap();
+        let context = fontmap.create_context().unwrap();
+        let layout = LayoutWrapper(pango::Layout::new(&context));
+        let font_desc = pango::FontDescription::from_string("Sans Bold 12");
+        layout.set_font_description(Some(&font_desc));
+
+        let drawer = Arc::new(Mutex::new(DrawingContext {
+            layout: glib::SendUniqueCell::new(layout).unwrap(),
+            info: None,
+        }));
+
+        let draw_args = Box::new(overlay_element);
+
+        // get a copy of the wrapper object so it can be moved into the callback
+        let drawer_clone = drawer.clone();
+        overlay
+            .connect("draw", false, move |args| {
+                let drawer = &drawer_clone;
+                let drawer = drawer.lock().unwrap();
+
+                let timestamp = args[2].get_some::<gst::ClockTime>().unwrap();
+                let ctx = args[1].get::<cairo::Context>().unwrap().unwrap();
+                let layout = drawer.layout.borrow();
+
+                // create an empty string as a placeholder for render time comparison
+                let mut time_str = String::from("");
+
+                // create new surface to draw on
+                let surface = cairo::ImageSurface::create(cairo::Format::Rgb30, 20, 20).unwrap();
+                ctx.set_source_surface(&surface, 0., 0.);
+
+                // format time as string to display on video frame
+                let timestamp_str = format!("{:.11}", timestamp.to_string());
+                ctx.set_source_rgba(1.0, 1.0, 1.0, 1.);
+                ctx.move_to(1800., 970.);
+                layout.set_text(&timestamp_str);
+                pangocairo::functions::show_layout(&ctx, &**layout);
+
+                // for mut i in 0..draw_args.len() {
+                //     println!("{}", i);
+                //     match draw_args[i] {
+                //         OverlayElement::Rectangle(mut rect) => {
+                //             ctx.set_source_rgba(rect.rgba.0, rect.rgba.1, rect.rgba.2, rect.rgba.3);
+                //             ctx.rectangle(rect.x, rect.y, rect.width, rect.height);
+                //             if !rect.rendered {
+                //                 if timestamp < rect.present_time.1 * gst::SECOND {
+                //                     ctx.fill();
+                //                 // break 'outer;
+                //                 } else {
+                //                     rect.rendered = true;
+                //                 }
+                //             } else {
+                //                 // ctx.fill();
+                //                 continue;
+                //             }
+                //         }
+                //         OverlayElement::Text => (),
+                //     }
+                // }
+
+                for ele in draw_args.iter() {
+                    match ele.clone() {
+                        OverlayElement::Rectangle(mut rect) => {
+                            if !rect.rendered {
+                                if (timestamp < rect.present_time.1 * gst::SECOND)
+                                    && (timestamp > rect.present_time.0 * gst::SECOND)
+                                {
+                                    ctx.set_source_rgba(
+                                        rect.rgba.0,
+                                        rect.rgba.1,
+                                        rect.rgba.2,
+                                        rect.rgba.3,
+                                    );
+                                    ctx.rectangle(rect.x, rect.y, rect.width, rect.height);
+                                    ctx.fill();
+                                } else {
+                                    rect.rendered = true;
+                                }
+                            } else {
+                                continue;
+                            }
+                        }
+                        OverlayElement::Text(mut text) => {
+                            if !text.rendered {
+                                if (timestamp < text.present_time.1 * gst::SECOND)
+                                    && (timestamp > text.present_time.0 * gst::SECOND)
+                                {
+                                    ctx.move_to(text.x, text.y);
+                                    ctx.set_source_rgba(
+                                        text.rgba.0,
+                                        text.rgba.1,
+                                        text.rgba.2,
+                                        text.rgba.3,
+                                    );
+                                    layout.set_text(text.content.as_str());
+                                    pangocairo::functions::show_layout(&ctx, &**layout);
+                                } else {
+                                    text.rendered = true;
+                                }
+                            } else {
+                                continue;
+                            }
+                        }
+                    }
+                }
+                None
+            })
+            .unwrap();
+
+        overlay
+            .connect("caps-changed", false, move |args| {
+                let _overlay = args[0].get::<gst::Element>().unwrap().unwrap();
+                let caps = args[1].get::<gst::Caps>().unwrap().unwrap();
+
+                let mut drawer_time = drawer.lock().unwrap();
+                drawer_time.info = Some(gst_video::VideoInfo::from_caps(&caps).unwrap());
+
+                None
+            })
+            .unwrap();
     }
 }
--- gst-draw_on_video/src/main.rs
+++ gst-draw_on_video/src/main.rs
@@ -2,14 +2,10 @@ extern crate vid_overlay;
 
 use gst::prelude::*;
 
-use pango::prelude::*;
-use std::ops;
-use std::sync::{Arc, Mutex};
-
 use anyhow::Error;
 use derive_more::{Display, Error};
 
-use vid_overlay::VideoContext;
+use vid_overlay::{OverlayElement, VideoContext};
 
 #[derive(Debug, Display, Error)]
 #[display(fmt = "Missing element {}", _0)]
@@ -24,137 +20,34 @@ struct ErrorMessage {
     source: glib::Error,
 }
 
-struct DrawingContext {
-    layout: glib::SendUniqueCell<LayoutWrapper>,
-    info: Option<gst_video::VideoInfo>,
-}
-
-#[derive(Debug)]
-struct LayoutWrapper(pango::Layout);
-
-impl ops::Deref for LayoutWrapper {
-    type Target = pango::Layout;
-
-    fn deref(&self) -> &pango::Layout {
-        &self.0
-    }
-}
-
-unsafe impl glib::SendUnique for LayoutWrapper {
-    fn is_unique(&self) -> bool {
-        self.0.ref_count() == 1
-    }
-}
-
-fn create_pipeline() -> Result<gst::Pipeline, Error> {
-    // gst::init()?;
-
-    // // create pipeline
-    // let pipeline = gst::Pipeline::new(None);
-
-    // // initiate elements
-    // let src = gst::ElementFactory::make("videotestsrc", None)
-    //     .map_err(|_| MissingElement("videotestsrc"))?;
-    // let overlay = gst::ElementFactory::make("cairooverlay", None)
-    //     .map_err(|_| MissingElement("cairooverlay"))?;
-    // let capsfilter =
-    //     gst::ElementFactory::make("capsfilter", None).map_err(|_| MissingElement("capsfilter"))?;
-    // let videoconvert = gst::ElementFactory::make("videoconvert", None)
-    //     .map_err(|_| MissingElement("videoconvert"))?;
-    // let sink = gst::ElementFactory::make("autovideosink", None)
-    //     .map_err(|_| MissingElement("autovideosink"))?;
-
-    // // link elements in the pipeline
-    // pipeline.add_many(&[&src, &overlay, &capsfilter, &videoconvert, &sink])?;
-    // gst::Element::link_many(&[&src, &overlay, &capsfilter, &videoconvert, &sink])?;
-
-    // // set input video format and pattern
-    // let caps = gst::Caps::builder("video/x-raw")
-    //     .field("width", &1920i32)
-    //     .field("height", &1080i32)
-    //     .field("framerate", &gst::Fraction::new(30, 1))
-    //     .build();
-    // capsfilter.set_property("caps", &caps).unwrap();
-    // src.set_property_from_str("pattern", "smpte");
-
+fn draw_elements() -> gst::Pipeline {
     let pipeline = VideoContext::new(1920i32, 1080i32, 30i32).unwrap();
     let overlay = pipeline.get_by_name("overlay").unwrap();
 
-    // initiate cairo draw element
-    let fontmap = pangocairo::FontMap::new().unwrap();
-    let context = fontmap.create_context().unwrap();
-    let layout = LayoutWrapper(pango::Layout::new(&context));
-    let font_desc = pango::FontDescription::from_string("Sans Bold 12");
-    layout.set_font_description(Some(&font_desc));
+    let rect =
+        OverlayElement::create_rectangle(670., 100., 300., 100., (0.2, 0.8, 1.0, 1.), (3, 6));
+    let rect2 =
+        OverlayElement::create_rectangle(1070., 200., 300., 100., (1.0, 0.3, 0.6, 1.), (5, 10));
+    let rect3 =
+        OverlayElement::create_rectangle(1070., 550., 300., 100., (0.1, 0.5, 1.0, 1.), (7, 13));
+    let rect4 =
+        OverlayElement::create_rectangle(1600., 700., 500., 50., (0.9, 0.5, 1.0, 1.), (4, 12));
+    let rect5 =
+        OverlayElement::create_rectangle(1800., 550., 900., 100., (0.1, 0.9, 1.0, 1.), (1, 8));
 
-    // wrapper around cairo element so it can be sent across threads
-    let drawer = Arc::new(Mutex::new(DrawingContext {
-        layout: glib::SendUniqueCell::new(layout).unwrap(),
-        info: None,
-    }));
+    let txt =
+        OverlayElement::create_text("Foo Bar".to_string(), 200., 0., (0.1, 0.5, 1.0, 1.), (0, 5));
+    let txt2 = OverlayElement::create_text(
+        "This is the longest paragraph ever to be typed and I can't think of anything else to type so I am just clickity clackity yappin' on keyboard".to_string(),
+        200.,
+        100.,
+        (0.1, 0.5, 1.0, 1.),
+        (0, 5),
+    );
 
-    // get a copy of the wrapper object so it can be moved into the callback
-    let drawer_clone = drawer.clone();
+    VideoContext::draw_on(overlay, vec![rect, rect2, rect3, rect4, rect5, txt, txt2]);
 
-    // draw callback - is called on every frame received
-    overlay
-        .connect("draw", false, move |args| {
-            // lock on wrapper object from program thread
-            let drawer = &drawer_clone;
-            let drawer = drawer.lock().unwrap();
-
-            // get callback arguments
-            let timestamp = args[2].get_some::<gst::ClockTime>().unwrap();
-            let ctx = args[1].get::<cairo::Context>().unwrap().unwrap();
-
-            let layout = drawer.layout.borrow();
-            // create new surface to draw on
-            let surface = cairo::ImageSurface::create(cairo::Format::Rgb30, 20, 20).unwrap();
-            ctx.set_source_surface(&surface, 0., 0.);
-
-            // format time as string to display on video frame
-            let timestamp_str = format!("{:.11}", timestamp.to_string());
-            ctx.set_source_rgba(1.0, 1.0, 0.0, 1.);
-            ctx.move_to(1800., 970.);
-            layout.set_text(&timestamp_str);
-            pangocairo::functions::show_layout(&ctx, &**layout);
-
-            // write "Foo Bar" on video frame
-            ctx.set_source_rgba(1.0, 0.5, 0.0, 1.);
-            ctx.move_to(0., 0.);
-            let msg = "Foo Bar";
-            layout.set_text(msg);
-            pangocairo::functions::show_layout(&ctx, &**layout);
-
-            // draw a rectangle on the video frame
-            ctx.set_source_rgba(1.0, 1.0, 1.0, 0.6);
-            ctx.rectangle(670., 0., 300., 100.);
-            ctx.fill();
-
-            // write "Baz Qux" on the video frame
-            ctx.move_to(670., 0.);
-            ctx.set_source_rgba(0.2, 0.5, 0.3, 1.);
-            let msg = "Baz Qux";
-            layout.set_text(msg);
-            pangocairo::functions::show_layout(&ctx, &**layout);
-
-            None
-        })
-        .unwrap();
-
-    overlay
-        .connect("caps-changed", false, move |args| {
-            let _overlay = args[0].get::<gst::Element>().unwrap().unwrap();
-            let caps = args[1].get::<gst::Caps>().unwrap().unwrap();
-
-            let mut drawer_time = drawer.lock().unwrap();
-            drawer_time.info = Some(gst_video::VideoInfo::from_caps(&caps).unwrap());
-
-            None
-        })
-        .unwrap();
-
-    Ok(pipeline)
+    pipeline
 }
 
 fn main_loop(pipeline: gst::Pipeline) -> Result<(), Error> {
@@ -192,7 +85,8 @@ fn main_loop(pipeline: gst::Pipeline) -> Result<(), Error> {
 }
 
 fn main() {
-    match create_pipeline().and_then(main_loop) {
+    let pipeline = draw_elements();
+    match main_loop(pipeline) {
         Ok(r) => r,
         Err(e) => eprintln!("Error! {}", e),
     }
